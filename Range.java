670bc1529a3848063200296d7b5a0b65cb2b268c
vli21 <vli21@stuy.edu>
2020-12-07 22:22:51 -0500

    works and tester

--- Range.java
+++ Range.java
@@ -18,5 +18,28 @@ public class Range implements IntegerSequence{
   public int length(){
     return ((this.end-this.start)+1);
   }
+  //When current is no longer a valid element in the range, it should return false.
+  public boolean hasNext(){
+    if (this.current >= this.start && this.current <= this.end){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //@throws NoSuchElementException when hasNext() is false.
+  //This will return the current value, it will also increase current value by 1.
+  //e.g.  if current is 5. This will make current 6, and return 5.
+  public int next(){
+    int prevCurrent= this.current;
+
+    if (hasNext()== false){
+      throw new NoSuchElementException();
+
+    }
+    this.current+=1;
+    return prevCurrent;
+  }
 
 }
